e9a5d645bdcf8950e6bf5f48e190c074529cce47
ArcticTheFox <71247575+ThatFoxWasTaken@users.noreply.github.com>
2022-03-11 12:25:09 +0100

    Add files via upload

---
+++ winget3beta.bat
@@ -0,0 +1,203 @@
+@echo off
+echo This project is open source, you can view the code on github (https://github.com/ThatFoxWasTaken/NewStart)
+:: Tells the user that this is an open source project
+
+echo .
+echo Trying to find windows's package manager...
+
+
+
+
+echo If you have problems with this EXE try opening this EXE with administrative priviliges
+:: Yeah this script needs admin priviliges cause winget is admin only
+
+echo Starting installation of Google Chrome using Windows Package Manager
+winget install Google.Chrome 
+:: current google chrome ID (At least in windows's package manager
+
+
+:: this part just cleans the cmd up a bit so the user sees it a bit better
+echo Clearing up...
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+
+:: I know this is not the best to do but i'm not the most familiar with bach script
+
+
+echo Starting installation of Steam using Windows Package Manager
+winget install Valve.Steam
+:: Starting install of steam
+
+echo Clearing up...
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+
+echo Starting installation of 7zip (zip program) using Windows Package Manager
+winget install 7zip.7zip
+:: Windows's zip manager kinda sucks so i just give the user 7zip 
+
+echo Clearing up...
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+
+echo Starting installation of WinRar using Windows Package Manager
+winget install RARLab.WinRAR
+:: everyone needs winrar right?
+
+echo Clearing up...
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+
+echo Starting installation of discord using Windows Package Manager
+winget install Discord.Discord
+:: Please use discord it's better than literaly anything else in my opinion
+
+echo Clearing up...
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+
+
+
+
+start chrome https://twitter.com/ArcticcTheFox
+:: Opens my twitter (the devs) pls folow thanks
+
+echo installation done, all the necessary starting out programs have been installed, thanks for using this script :)
+echo I made this script for anyone wanting a quick install of all the programs that they need, this is completely free but if you would be generous please follow my twitter
+pause
+
